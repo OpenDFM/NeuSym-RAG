eaa05af12df388816e26c68696ef032fdda31c39
rhythmcao <ruishengcao@gmail.com>
2024-12-05 18:04:04 +0800

    add try ... catch for eval airqa

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,9 +1,20 @@
 #coding=utf8
-import os, sys
+import os, sys, logging, traceback
 from typing import Dict, Any
 # Add the parent directory to the path so that we can import the evaluation module
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import evaluation
+
+
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    fmt='[%(asctime)s][%(filename)s - %(lineno)d][%(levelname)s]: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 
 def evaluate_airqa(pred_answer: str, gold: Dict[str, Any]) -> float:
@@ -13,4 +24,10 @@ def evaluate_airqa(pred_answer: str, gold: Dict[str, Any]) -> float:
     eval_func = getattr(evaluation, function_name, None)
     assert eval_func is not None, f"Evaluation function `{function_name}` not found in the evaluation module. Remember to import it in the evaluation/__init__.py file."
     eval_kwargs = gold['evaluator']['eval_kwargs']
-    return eval_func(pred_answer, **eval_kwargs)
+    try:
+        score = eval_func(pred_answer, **eval_kwargs)
+    except Exception as e:
+        score = 0.0
+        logger.error(traceback.format_exc())
+        logger.error('Error occurred during evaluation: %s', str(e))
+    return score
